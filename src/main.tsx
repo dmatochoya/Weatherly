97092ee27b520fa5f65e9a59211c9ebbfb581f34
David <dmatochoyaprojects@gmail.com>
2024-09-10 19:21:41 +0200

    feat: add routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,28 @@
 import { StrictMode } from "react";
+import { ThemeProvider } from "styled-components";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import GlobalStyle from "./styles/GlobalStyle";
+import theme from "./styles/theme";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    // TODO: Add ErrorBoundary
+  },
+  {
+    path: "dashboard",
+    element: <div>Dashboard</div>,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>
 );
