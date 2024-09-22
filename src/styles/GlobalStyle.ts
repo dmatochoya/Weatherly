d64d6d6a0e063c390b1868468bd0bb88f739bd52
David <dmatochoyaprojects@gmail.com>
2024-09-22 20:51:25 +0200

    feat: remove focus outline on touch devices using @media (hover: none) in GlobalStyles

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,27 +1,33 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-    *, *::before, *::after {
-      box-sizing: border-box;
-    }
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
 
-    * {
-      margin: 0;
-    }
+  * {
+    margin: 0;
+  }
 
-    html {
-      font-size: clamp(8px, calc(8px + 0.6vw), 16px)
-    }
+  html {
+    font-size: clamp(8px, calc(8px + 0.6vw), 16px)
+  }
 
-    body {
-      font-family: ${({ theme }) => theme.fontFamily.main};
-      background-color: ${({ theme }) => theme.colors.background};
-    }
+  body {
+    font-family: ${({ theme }) => theme.fontFamily.main};
+    background-color: ${({ theme }) => theme.colors.background};
+  }
 
-    button {
-      border: none;
-      cursor: pointer;
+  button {
+    border: none;
+    cursor: pointer;
+  }
+
+  @media (hover: none) {
+    button:focus {
+      outline: none;
     }
+  }
 `;
 
 export default GlobalStyle;
